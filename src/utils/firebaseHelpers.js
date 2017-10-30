be1d1dc3b4bccf2e31ac4b403f8a4c74a2b3a6a4
Steven Anderson <sdandersonz97@gmail.com>
2017-10-30 15:21:51 -0400

    feat: secondaries ref to firebase database

--- src/utils/firebaseHelpers.js
+++ src/utils/firebaseHelpers.js
@@ -1,5 +1,7 @@
 import firebase from 'firebase'
 
+
+
 //Primary refs
 export const rootRef = () => firebase.database().ref()
 export const usersRef = () => rootRef().child('users')
@@ -7,3 +9,8 @@ export const quizesRef = () => rootRef().child('quizez')
 export const questionsRef = () => rootRef().child('questions')
 export const tagsRef = () => rootRef().child('tags')
 
+//Secondary refs
+export const userRef = uid => usersRef().child(uid)
+export const quizRef = quizId => quizesRef().child(quizId)
+export const questionRef = questionId => questionsRef().child(questionId)
+export const tagRef = tagId => tagsRef().child(tagId)

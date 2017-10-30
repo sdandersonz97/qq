4b550d0e9e7e50fb9fbcee25563b6e7d29df3d51
Steven Anderson <sdandersonz97@gmail.com>
2017-10-30 15:39:16 -0400

    feat: provider wired up

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Text, View } from 'react-native'
-
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import rootReducer from './src/reducers'
 export default class App extends React.Component {
   render() {
+    const store = createStore(rootReducer, applyMiddleware(thunk))
     return (
-      <View>
-        <Text>Quizlo!</Text>
-      </View>
+      <Provider store={store}>
+        <View>
+          <Text>Quizlo!</Text>
+        </View>
+      </Provider>
     )
   }
 }
